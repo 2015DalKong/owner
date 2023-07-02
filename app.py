dc9a3bed076d4fc0475fc3be3747cc63b55fc4bc
2015DalKong <109940012+2015DalKong@users.noreply.github.com>
2023-07-02 09:03:13 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 import streamlit as st
 
 view = [100,150,30]
+view1 = [20,40,60]
+
 st.write("# Youtube view")
 st.write('## raw')
 view
+view1
 st.write('## bar chart')
 st.bar_chart(view)
+st.bar_chart(view1)
 
 import pandas as pd
 sview = pd.Series(view)
